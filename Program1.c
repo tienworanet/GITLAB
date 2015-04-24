dbb84836eb6db71c7bfcb77a9922d49feb62058a
kitdacloud <kitda_pee@hotmail.com>
2015-04-24 14:40:40 +0700

    userBchange(18)

--- Program1.c
+++ Program1.c
@@ -9,6 +9,11 @@ void main()
 	scanf("%s", name_b);
      printf("What is userC's name? ");
 	scanf("%s", name_c);
-	printf("UserA_Name: %s\nUserB_Name: %s\nUserC_Name: %s", name_a, name_b, name_c);
+	printf("What is userB's name? ");
+	scanf("%s", name_d);
+	printf("What is userC's name? ");
+	scanf("%s", name_e);
+	printf("UserA_Name: %s\nUserB_Name: %s\nUserC_Name: %s", name_a, name_b, name_c, name_d, name_e );
+	printf("edit by USER B ");
      
 }
--- Program2.c
+++ Program2.c
@@ -1,4 +1,6 @@
 #include<sudio.h>
 void main(){
 	printf("hello world Edit 1");
+	printf("Edit program 2 by");
+	printf("User B");
 }
