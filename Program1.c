005376f03af8fbc8404a5115772919b33e1b759e
tienworanet <tienworanet@gmail.com>
2015-04-24 14:40:15 +0700

    userAchange(18)

--- Program1.c
+++ Program1.c
@@ -3,12 +3,10 @@
 void main()
 {
 	char name_a[40],name_b[40],name_c[40];
-	printf("What is userA's name? ");
-	scanf("%s", name_a);
 	printf("What is userB's name? ");
 	scanf("%s", name_b);
      printf("What is userC's name? ");
 	scanf("%s", name_c);
-	printf("UserA_Name: %s\nUserB_Name: %s\nUserC_Name: %s", name_a, name_b, name_c);
+	printf("UserB_Name: %s\nUserC_Name: %s", name_b, name_c);
      
 }
