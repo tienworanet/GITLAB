e08678a3b9185a62d691734bce02e7ab0591344c
tienworanet <tienworanet@gmail.com>
2015-04-24 13:33:25 +0700

    mod testgitlab.c(Program1)

--- testgitlab.c
+++ testgitlab.c
@@ -7,8 +7,8 @@ void main()
 	scanf("%s", name_a);
 	printf("What is userB's name? ");
 	scanf("%s", name_b);
-     //printf("What is userC's name? ");
-	//scanf("%s", name_c);
-	printf("UserA_Name: %s\nUserB_Name: %s\nUserC_Name: %s", name_a, name_b);
+     printf("What is userC's name? ");
+	scanf("%s", name_c);
+	printf("UserA_Name: %s\nUserB_Name: %s\nUserC_Name: %s", name_a, name_b, name_c);
      
 }
